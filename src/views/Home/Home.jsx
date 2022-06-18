53e22f8a1f487a531b91c438cd80f786218b0bee
anhnhanle1905 <75842089+anhnhanle1905@users.noreply.github.com>
2022-06-18 10:50:29 +0700

    fix web

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -34,7 +34,7 @@ export default function Home() {
         <ButtonScrollTop />
         <FlashSale />
         <FavoritePlace />
-        <Fly />
+        {/* <Fly /> */}
         <ButtonCard />
       </main>
       <Footer />
