39279b5b2368bbf0be1a041ab29e8688375f77c5
anhnhanle1905 <75842089+anhnhanle1905@users.noreply.github.com>
2022-06-20 00:06:33 +0700

    Final Project

--- src/components/FavoriteRoom/index.jsx
+++ src/components/FavoriteRoom/index.jsx
@@ -60,7 +60,7 @@ function FavoriteRoom() {
           );
         })}
       </div>
-      <div className="mt40 a_center">
+      <div className="mt40 a_center" style={{ marginBottom: "50px" }}>
         <a className="button btn_sm btn_yellow" href="/ListRoom">
           VIEW ROOMS LIST
         </a>
