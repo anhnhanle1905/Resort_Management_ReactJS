29474867273984d35eddd1bf3d1140f2144146b0
anhnhanle1905 <75842089+anhnhanle1905@users.noreply.github.com>
2022-06-19 12:48:48 +0700

    Fix from nhan branch

--- src/api/loginApi.js
+++ src/api/loginApi.js
@@ -1,16 +1,19 @@
-import axios from "axios"
-const url = 'http://localhost:3030/auth/'
+import axios from "axios";
+const url = "http://localhost:3030/auth/";
 
+export const loginApi = async (data) => {
+  try {
+    const res = await axios.post(`${url}login`, data);
+    if (res) {
+      localStorage.setItem("idUser", res?.data?.user?._id);
+      localStorage.setItem("fullname", res?.data?.user?.fullname);
+      localStorage.setItem("token", res?.data?.token);
+      localStorage.setItem("statusLogin", true);
+      //note
 
- export const loginApi = async (data) =>{
-    try {
-        const res = await axios.post(`${url}login`,data)
-        if(res){
-            localStorage.setItem('idUser',res?.data?.user?._id)
-            localStorage.setItem("token",res?.data?.token)
-            return true
-        }
-    } catch (error) {
-       return false
+      return true;
     }
-}
+  } catch (error) {
+    return false;
+  }
+};
